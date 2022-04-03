4e0505be7286bb61e0a06354ab0089494441cd45
zca93 <zca93@sfu.ca>
2022-04-03 08:50:13 +0000

    Update RegularRewardTest.java

--- src/test/java/RegularRewardTest.java
+++ src/test/java/RegularRewardTest.java
@@ -1,19 +1,41 @@
-import com.CMPT276_Group1.project.*;
+import com.CMPT276_Group1.project.GamePanel;
+import com.CMPT276_Group1.project.UtilityTool;
+import com.CMPT276_Group1.project.object.RegularReward;
 import org.junit.jupiter.api.*;
 
-import java.awt.event.*;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
-
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RegularRewardTest {
+    private GamePanel GP;
+    private RegularReward r;
+    @BeforeEach
+    public void setUp(){
+        GP=new GamePanel();
+        GP.setupGameObject();
+        r= (RegularReward) GP.obj[0];
+    }
     @Test
     public void testImage() {
-        File file = new File("regular_reward.png");
+        /*File file = new File("regular_reward.png");
         assertNotNull(file,"asserts that the image file is not null");
-        assertEquals("regular_reward.png",file.getName(),"asserts that the image file is correct");
+        assertEquals("regular_reward.png",file.getName(),"asserts that the image file is correct");*/
+        BufferedImage test = r.setImage("regular_reward");
+        BufferedImage image;
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/objects/regular_reward.png"));
+            assertNotNull(test,"asserts that the image file is not null");
+            assertEquals(test.getHeight(),image.getHeight(),"asserts that the image file height is correct");
+            assertEquals(test.getWidth(),image.getWidth(),"asserts that the image file width is correct");
+            assertEquals(test.getType(),image.getType(),"asserts that the image file type is correct");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-}
+    }
 
